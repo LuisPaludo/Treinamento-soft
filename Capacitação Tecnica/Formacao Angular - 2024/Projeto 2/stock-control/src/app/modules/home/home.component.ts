007eeb507fbebd8045630edbe6c1c22838ce166f
Luis Paludo <luis.paludo@softfocus.com.br>
2024-02-26 15:43:04 -0300

    Projeto 2 de Angular implementado até seção de logout

---
+++ Capacitação Tecnica/Formacao Angular - 2024/Projeto 2/stock-control/src/app/modules/home/home.component.ts
@@ -0,0 +1,101 @@
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+import { AuthRequest } from 'src/app/models/interfaces/user/auth/AuthRequest';
+import { UserService } from 'src/app/services/user/user.service';
+import { SignUpUserRequest } from 'src/app/models/interfaces/user/SignUpUserRequest';
+
+import { CookieService } from 'ngx-cookie-service';
+import { MessageService } from 'primeng/api';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+  loginCard = true;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService,
+    private cookieService: CookieService,
+    private messageService: MessageService,
+    private router: Router,
+    ) {}
+
+  loginForm = this.formBuilder.group({
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  signUpForm = this.formBuilder.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmitLoginForm():void {
+    if(this.loginForm.value && this.loginForm.valid) {
+      this.userService.authUser(this.loginForm.value as AuthRequest).subscribe({
+        next: (response) => {
+          if (response) {
+            this.cookieService.set('USER_INFO', response?.token);
+            
+            this.loginForm.reset();
+
+            this.router.navigate(['/dashboard']);
+
+            this.messageService.add({
+              severity: 'success',
+              summary:'Sucesso',
+              detail: `Bem vindo de volta ${response?.name}!`,
+              life: 2000,
+            })
+          }
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary:'Erro',
+            detail: `Erro ao fazer o login!`,
+            life: 2000,
+          })
+        }
+      })
+    }
+  }
+
+  onSubmitSignUpForm():void {
+    if (this.signUpForm.valid && this.signUpForm.value) {
+      this.userService.signUpUser(
+        this.signUpForm.value as SignUpUserRequest
+      ).subscribe({
+        next: (response) => {
+          if(response) {
+            this.signUpForm.reset();
+
+            this.messageService.add({
+              severity: 'success',
+              summary:'Sucesso',
+              detail: `Usuário criado com sucesso!`,
+              life: 2000,
+            })
+
+            this.loginCard = true;
+          }
+        },
+        error: (err) => {
+          this.messageService.add({
+            severity: 'error',
+            summary:'Erro',
+            detail: `Erro ao criar usuário!`,
+            life: 2000,
+          })
+
+        }
+      });
+    }
+  }
+}
---
+++ Capacitação Tecnica/Formacao Angular - 2024/Projeto 2/stock-control/src/app/services/user/user.service.ts
@@ -0,0 +1,34 @@
+import { AuthRequest } from './../../models/interfaces/user/auth/AuthRequest';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+import { SignUpUserRequest } from 'src/app/models/interfaces/user/SignUpUserRequest';
+import { SignUpUserResponse } from 'src/app/models/interfaces/user/SignUpUserResponse';
+import { AuthResponse } from 'src/app/models/interfaces/user/auth/AuthResponse';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient, private cookie: CookieService) { }
+
+  signUpUser(requestDatas: SignUpUserRequest): Observable<SignUpUserResponse> {
+    return this.http.post<SignUpUserResponse>(
+      `${this.API_URL}/user`,
+      requestDatas
+    )
+  }
+
+  authUser(requestDatas: AuthRequest): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.API_URL}/auth`, requestDatas);
+  }
+
+  isLoggedIn():boolean {
+    const JWT_TOKEN = this.cookie.get('USER_INFO');
+    return JWT_TOKEN ? true : false;
+  }
+}
