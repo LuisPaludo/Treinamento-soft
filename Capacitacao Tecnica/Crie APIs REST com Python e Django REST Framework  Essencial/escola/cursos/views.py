d0cb11ccc099e6acb85f7668d8314a9e30018707
Luis Paludo <luis.paludo@softfocus.com.br>
2024-03-07 14:42:14 -0300

    50% curso de RES Intermediário

--- Capacitacao Tecnica/Crie APIs REST com Python e Django REST Framework  Essencial/escola/cursos/views.py
+++ Capacitacao Tecnica/Crie APIs REST com Python e Django REST Framework  Essencial/escola/cursos/views.py
@@ -1,9 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics, mixins, viewsets
+from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
 
 from .models import Avaliacao, Curso
 from .serializers import AvaliacaoSerializer, CursoSerializer
 
+"""
+API V1
+"""
 
 class CursosAPIView(generics.ListCreateAPIView):
     queryset = Curso.objects.all()
@@ -38,3 +43,31 @@ class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
             self.get_queryset(),
             pk=self.kwargs.get('avaliacao_pk')
         )
+    
+"""
+API V2
+"""
+
+class CursoViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    @action(detail=True, methods=['get'])
+    def avaliacoes(self, request, pk=None):
+        curso = self.get_object()
+        serializer = AvaliacaoSerializer(curso.avaliacoes.all(), many=True)
+        return Response(serializer.data)
+
+# class AvaliacaoViewSet(viewsets.ModelViewSet):
+#     queryset = Avaliacao.objects.all()
+#     serializer_class = AvaliacaoSerializer
+    
+class AvaliacaoViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
